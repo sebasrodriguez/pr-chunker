1438f7a156938391fe65e9257f72d0c47df11d1e
Sebastián Rodríguez <sebastian.rodriguez@jam3.com>
2021-06-29 17:50:44 -0300

    wip: Updating action

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
 
 console.log(`Creating PR if it exceeds ${core.getInput('max-diff')}`);
-console.log(`Creating PR if it exceeds ${core.getInput('staging-branch')}`);
+console.log(`Diffing against ${core.getInput('staging-branch')}`);
+
+exec.exec(`git diff ${core.getInput('staging-branch')}`);
