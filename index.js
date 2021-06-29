fd516ddee151e341f064b9d3b6717a0a5c74af6b
Sebastián Rodríguez <srodriguez@MVAPC02FG1NTML7L.local>
2021-06-29 17:44:17 -0300

    wip: Updating action

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const core = require('@actions/core');
 
 console.log(`Creating PR if it exceeds ${core.getInput('max-diff')}`);
+console.log(`Creating PR if it exceeds ${core.getInput('staging-branch')}`);
