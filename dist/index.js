a45d8cfb44b64bd847747ad7af3cacde403d44c9
Sebastián Rodríguez <sebastian.rodriguez@jam3.com>
2021-07-01 13:58:16 -0300

    wip: Updating action

--- dist/index.js
+++ dist/index.js
@@ -1858,18 +1858,107 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(186);
 const exec = __nccwpck_require__(514);
 
-console.log(`Creating PR if it exceeds ${core.getInput("max-diff")}`);
-console.log(`Diffing against ${core.getInput("staging-branch")}`);
+const limit = +core.getInput("max-diff") - +core.getInput("buffer");
+const baseBranch = core.getInput("base-branch");
+console.log(`Creating PR if it exceeds ${limit}`);
+console.log(`Diffing against ${core.getInput("base-branch")}`);
 
-exec
-  .getExecOutput(
-    `git diff --shortstat origin/${core.getInput("staging-branch")}`
-  )
-  .then((value) => {
-    console.log(value.stdout);
-  });
+const getMissingCommits = async (baseBranch, branch) => {
+  const { stdout } = await exec.getExecOutput(
+    `git log ${baseBranch} ^${branch} --format=format:%H`
+  );
 
-exec.exec('gh --help');
+  const result = stdout.split("\n").reverse();
+
+  return result;
+};
+
+const getDiff = async (origin, target) => {
+  const { stdout } = await exec.getExecOutput(
+    `git diff ${target}..${origin} --shortstat`
+  );
+  const regExp =
+    /((?<insertions>\d+)\sinsertions)|((?<deletion>\d+)\sdeletion)/;
+  const match = regExp.exec(stdout);
+
+  const totalDiff =
+    (+match.groups.deletion || 0) + (+match.groups.insertions || 0);
+
+  return totalDiff;
+};
+
+const getPRCommit = async () => {
+  const missingCommits = await getMissingCommits("develop", "staging");
+
+  for (let index = 0; index < missingCommits.length; index++) {
+    const localDiff = await getDiff(missingCommits[index], "staging");
+
+    if (localDiff >= limit) {
+      const commitIdForPR =
+        index === 0 ? missingCommits[0] : missingCommits[index - 1];
+
+      return commitIdForPR;
+    }
+  }
+};
+
+const createBranchIfNotExists = async (commitId) => {
+  let exists = false;
+  const branch = `auto-merger/${commitId}`;
+  try {
+    const { stdout } = await exec.getExecOutput(
+      `git rev-parse --verify ${branch}`
+    );
+    exists = stdout.includes(commitId);
+  } catch (e) {
+    exists = false;
+  }
+
+  if (exists) {
+    console.info("AutoMerger: Branch already exists, skipping creation");
+  } else {
+    await exec.exec(`git branch ${branch} ${commitId}`);
+    await exec.exec(`git push -u origin ${branch}`);
+  }
+
+  return branch;
+};
+
+const createPRIfNotExists = async (branch, commitId) => {
+  const searchOutput = await exec.getExecOutput(
+    `gh pr list --search "body:'${commitId}'"`
+  );
+
+  if (searchOutput.stdout.includes("No pull requests match your search")) {
+    const createOutput = await exec.getExecOutput(
+      `gh pr create --base staging --title "[AutoMerger]: ${commitId}" --body "${commitId}" --head "${branch}"`
+    );
+  } else {
+    console.info("AutoMerger: PR already exists, skipping creation");
+  }
+};
+
+const run = async () => {
+  try {
+    const diff = await getDiff("develop", baseBranch);
+
+    if (diff >= limit) {
+      console.log(
+        `AutoMerger: Should create pr because we have ${diff} lines changed`
+      );
+      const commitIdForPR = await getPRCommit();
+      const branch = await createBranchIfNotExists(commitIdForPR);
+      await createPRIfNotExists(branch, commitIdForPR);
+    } else {
+      console.log("AutoMerger: Skipping automatic pr creation");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+run();
+
 })();
 
 module.exports = __webpack_exports__;
